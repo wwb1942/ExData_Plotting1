6b098aa60b31184866ad802d45580988ba8814dd
wwb1942 <515320899@qq.com>
2016-08-28 10:13:47 +0800

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -18,13 +18,28 @@ houseHoldConsumption$Time_stamp <- as.POSIXct(strptime(paste(houseHoldConsumptio
 
 sample <- filter(houseHoldConsumption,Time_stamp >= '2007-02-01 00:00:00' & Time_stamp <= '2007-02-02 23:59:59')
 
+t_subset<-select(sample,Time_stamp,Global_active_power)
+t_subset$Global_active_power<-as.numeric(t_subset$Global_active_power)/500
 
+t2_subset<-select(sample,Time_stamp,Sub_metering_1,Sub_metering_2,Sub_metering_3)
+t2_subset$Sub_metering_1<-(as.numeric(t2_subset$Sub_metering_1)-2)
+t2_subset$Sub_metering_2<-(as.numeric(t2_subset$Sub_metering_2)-2)
+t2_subset$Sub_metering_3<-(as.numeric(t2_subset$Sub_metering_3))
 
 
 t3_subset<-select(sample,Time_stamp,Voltage)
 t3_subset$Voltage<-as.numeric(t3_subset$Voltage)
 t4_subset<-select(sample,Time_stamp,Global_reactive_power)
 t4_subset$Global_reactive_power<-as.numeric(t4_subset$Global_reactive_power)/500
+
+ts0<-zoo(t_subset$Global_active_power,t_subset$Time_stamp)
+
+ts<-zoo(t2_subset$Sub_metering_1,t2_subset$Time_stamp)
+ts1<-zoo(t2_subset$Sub_metering_2,t2_subset$Time_stamp)
+ts2<-zoo(t2_subset$Sub_metering_3,t2_subset$Time_stamp)
+
+z <- cbind( zoo(t2_subset$Sub_metering_1,t2_subset$Time_stamp),t2_subset$Sub_metering_2, t2_subset$Sub_metering_3)
+
 
 ts3<-zoo(t3_subset$Voltage,t3_subset$Time_stamp)
 ts4<-zoo(t4_subset$Global_reactive_power,t4_subset$Time_stamp)
